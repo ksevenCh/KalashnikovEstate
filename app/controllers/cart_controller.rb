b1b39eb8b06e23620e38f2cff3c8a7e86136bf2b
ksevenCh <92891752+ksevenCh@users.noreply.github.com>
2023-12-20 20:52:37 +0300

    AddItem works now
    Co-Authored-By: Alexey <67546108+OrfeusPrice@users.noreply.github.com>

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -21,13 +21,14 @@ class CartController < ApplicationController
     return unless session[:user_id]
 
     @user = User.find_by(id: session[:user_id])
-    @good = Good.find_by(id: 1)
-    @cart_part = CartPart.new(quantity: 1)
-    @cart_part.user_id = @user.id
-    @cart_part.good_id = @good.id
-    @cart_part.save
+    @selected_good = Good.find_by(id: params[:good_id]) # Получаем id товара из параметров запроса
+    @cart_part = CartPart.new(user_id: @user.id, good_id: @selected_good.id, quantity: 1)
+    if @cart_part.save && @cart_part.good_id?
+      redirect_to controller: :products, action: :catalog, notice: "Товар успешно добавлен"
+    else
+      redirect_to controller: :products, action: :catalog, notice: "Товар не добавлен"
+    end
 
-      redirect_to controller: :cart, action: :cart, notice: "Товар успешно добавлен"
   end
   def cart_part_params
     params.require(:cart_part).permit(:quantity)
